33339ea65d518e89d408ecc7f3f2d3242bcb31c7
MFine <1737374338@qq.com>
2022-04-19 00:14:08 +0800

    proxy has a problem

--- src/core/MonitorObject.ts
+++ src/core/MonitorObject.ts
@@ -3,20 +3,17 @@ import DeepProxy from 'proxy-deep';
 import { doc } from 'prettier';
 import debug = doc.debug;
 
-
 class MonitorObject {
-
-  warpScene(scene:Scene):Scene{
-       return new DeepProxy<Scene>(scene, {
-           get(target: Scene, p: PropertyKey, receiver: any): any {
-               return this.nest(function() {
-               });
-           },
-           apply(target: Scene, thisArg: any, argArray?: any): any {
-               return this.path;
-           },
-       });
-   }
+    warpScene(scene: Scene): Scene {
+        return new DeepProxy<Scene>(scene, {
+            get(target: Scene, p: PropertyKey, receiver: any): any {
+                return this.nest(function () {});
+            },
+            apply(target: Scene, thisArg: any, argArray?: any): any {
+                return this.path;
+            },
+        });
+    }
 }
 const monitorObject = new MonitorObject();
 export default monitorObject;
