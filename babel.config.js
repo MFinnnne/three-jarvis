dc9d92d53635b1b9b5b6bd4649ab439c7f345ff0
MFine <1737374338@qq.com>
2022-06-08 19:41:20 +0800

    Begin to implement the import function

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-    presets: [['@babel/preset-env', {
-        modules: false,
-        targets: {
-            node: 'current',
-        }
-    }], '@babel/preset-typescript', 'babel-preset-million'],
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                modules: false,
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+        '@babel/preset-typescript',
+        'babel-preset-million',
+    ],
     plugins: [
         '@babel/plugin-transform-modules-commonjs',
         [
-            "@babel/plugin-transform-react-jsx",
+            '@babel/plugin-transform-react-jsx',
             {
-                "runtime": "automatic",
-                "importSource": "million"
-            }
-        ]
+                runtime: 'automatic',
+                importSource: 'million',
+            },
+        ],
     ],
 };
