73ffa1906634f5f3e597263a7b7e15bf8b92e6b3
MFine <lxemyf@gmail.com>
2023-01-18 15:15:35 +0800

    身体不舒服 休息两天再重构

---
+++ src/Threejarvis.ts
@@ -0,0 +1,91 @@
+import ObjectEventBus from "./core/ObjectEventBus";
+import Jarvis from "./core/Jarvis";
+import Toast from "./app/Toast";
+import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {FileLoader} from "three";
+import sceneDB, {SceneEntity} from "./core/mapper/SceneDB";
+
+type JarvisHook = {
+    afterRender?: () => void;
+    beforeRender?: () => void;
+    dataGet?: () => string;
+    dataStore?: (content: string) => void;
+};
+
+export default class ThreeJarvis {
+
+    private container: HTMLCanvasElement;
+
+    private objectEvents: ObjectEventBus[] = []
+
+    private jarvis?: Jarvis;
+
+    constructor(container: HTMLCanvasElement) {
+        if (container.id === undefined) {
+            Toast.show('container id  must be set and only');
+            throw new Error();
+        }
+        this.container = container;
+    }
+
+    public monitor(
+        scene: THREE.Scene,
+        camera: THREE.PerspectiveCamera,
+        renderer: THREE.WebGLRenderer,
+        options?: {
+            control?: OrbitControls;
+        },
+    ) {
+        const creator = new Jarvis(this.objectEvents);
+        creator.monitor(scene, renderer, camera, {
+            control: options?.control ?? new OrbitControls(camera, renderer.domElement),
+        });
+    }
+
+    public create(hook?: JarvisHook) {
+        this.jarvis = new Jarvis(this.objectEvents);
+        this.jarvis.creator(this.container).then(r => {
+        });
+    }
+
+    public createFrom(from: string | (() => string | ArrayBuffer), options?: JarvisHook) {
+        let creator: Jarvis;
+        const loader = new FileLoader();
+        if (typeof from === 'string') {
+            loader.loadAsync(from).then(res => {
+                let rawString: string;
+                if (typeof res !== 'string') {
+                    rawString = new TextDecoder().decode(res);
+                } else {
+                    rawString = res;
+                }
+                const se = JSON.parse(rawString) as SceneEntity;
+                creator = new Jarvis(this.objectEvents);
+                creator.creator(this.container, se).then(r => {
+                });
+            });
+        } else {
+            const data = from();
+            if (typeof data === 'string') {
+                const exist = data;
+                if (exist) {
+                    console.warn("this json has already exist in indexed db,we will select indexedDB's json");
+
+                } else {
+                    const parse = JSON.parse(data) as SceneEntity;
+                    sceneDB.addJson(parse);
+                }
+            }
+            creator = new Jarvis(this.objectEvents);
+            creator.creator(this.container).then(r => {
+            });
+        }
+    }
+
+
+    public subscribeByUUID(uuid: string): ObjectEventBus {
+        const objectEventDispatch = new ObjectEventBus('uuid', uuid);
+        return objectEventDispatch;
+    }
+}
--- src/core/Creator.ts
+++ src/core/Creator.ts
@@ -1,3 +1,34 @@
-export default class Creator {
+import ObjectEventBus from "./ObjectEventBus";
 
+export default class Creator {
+    constructor() {
+    }
+
+    public subscribeByUUID(uuid: string): Notify {
+        const eventBus = new ObjectEventBus('uuid', uuid);
+        return new Notify(eventBus, this);
+    }
+
+    public render(): void {
+
+    }
 }
+
+class Notify {
+    private readonly _bus: ObjectEventBus;
+    private readonly _creator: Creator;
+
+    constructor(bus: ObjectEventBus, creator: Creator) {
+        this._bus = bus;
+        this._creator = creator;
+    }
+
+    public on(callBack: (bus: ObjectEventBus) => void) {
+        callBack(this._bus);
+        return this._creator;
+    }
+}
+new Creator()
+    .subscribeByUUID('123').on(res=>{})
+    .subscribeByUUID("456").on(res=>{})
+    .render();
